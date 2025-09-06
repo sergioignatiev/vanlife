50cfaa4932cc3dc1e4c8206b9792a1fbf3e46a12
SergioIgnatev <ignatevsergey17@gmail.com>
2025-09-06 13:23:08 +0300

    dashboard little changes

--- src/views/host/TheVansView.vue
+++ src/views/host/TheVansView.vue
@@ -3,7 +3,7 @@
  <h1>Your listed vans</h1>
  <section class="flex flex-col gap-4 mt-[32px] ">
   <router-link  :to="{name:'van-details',params:{id:van.id}}" v-for="van in data"
-   class="flex items-center text-black no-underline rounded-md gap-4 bg-white h-[102px] p-[24px]" :key="van.id">
+   class="flex items-center text-black no-underline rounded-md gap-4 bg-white  px-[18px] py-[12px]  sm:px-[28px] sm:py-[18px]" :key="van.id">
     <img width="66" height="66" class="rounded-md" :src="van.imageUrl" :alt="van.name||'van'" loading="lazy">
     <div class="flex flex-col gap-2">
       <h2 class="text-[20px]">{{ van.name }}</h2>
